09691b2fa8d86af69109915f05e6361f30c4982e
Paulo Mazzia <pmazzia@gmail.com>
2019-08-21 14:25:32 -0300

    Inicial Commit

---
+++ src/Hello.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+/*
+function Hello() {
+    return (
+        <h2>Teste React</h2>
+    );
+}
+
+export default Hello;
+*/
+
+export default class Hello {
+    render() {
+        return (
+            <h2>Class React</h2>
+        );
+    }
+}
